361b4aa25ebce65229cd36a007d87c4dd5369cda
Chirayu1411Mitra <mitrachirayu@gmail.com>
2024-11-28 13:07:04 +0530

    .

---
+++ src/Push_zeros_to_end.java
@@ -0,0 +1,5 @@
+public class Push_zeros_to_end {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Second_largest_in_array.java
+++ src/Second_largest_in_array.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Second_largest_in_array {
+public class Second_largest_in_array {
+    static int Second_largest(int[] arr){
+        int max=0;
+       int max2=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max2=max;
+                max=arr[i];
+            }
+            else if(arr[i]>max2 && arr[i]!=max){
+
+                max2=arr[i];
+
+            }
+            else if(max2==0){
+                return -1;
+            }
+
+        }
+        return max2;
+    }
+    public static void main(String[] args) {
+        int[] arr={10,10,10};
+        int max =  Second_largest(arr);
+        System.out.println(max);
+    }
 }
